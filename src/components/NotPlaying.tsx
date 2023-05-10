5faeaf5eabea7e18cf2f9711f809725ec9c17b93
Karl Shea <karl@weilstreet.com>
2023-05-10 00:05:54 -0500

    Backlight control

--- src/components/NotPlaying.tsx
+++ src/components/NotPlaying.tsx
@@ -1,14 +1,47 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const ONE_SECOND = 1000;
+
+const BACKLIGHT_TIMEOUT = import.meta.env.VITE_BACKLIGHT_TIMEOUT;
 
 const NotPlaying = () => {
   const [date, setDate] = useState(new Date());
 
-  const refreshClock = () => setDate(new Date());
-
+  // Show clock while waiting for timeout.
   useEffect(() => {
-    const timerId = setInterval(refreshClock, 1000);
+    const refreshClock = () => setDate(new Date());
+
+    const timerId = setInterval(refreshClock, ONE_SECOND);
     return () => {
       clearInterval(timerId);
+    };
+  }, []);
+
+  // Set backlight power.
+  const setBacklightPowered = (power: boolean) =>
+    axios
+      .get(import.meta.env.VITE_BACKLIGHT_URL, {
+        params: { power: power ? 'on' : 'off' },
+      })
+      .catch((error: unknown) => {
+        console.log(error);
+      });
+
+  // Disable backlight after the timeout, and re-enable it on component unmount.
+  useEffect(() => {
+    const disableBacklight = () => {
+      setBacklightPowered(false);
+    };
+    const enableBacklight = () => {
+      setBacklightPowered(true);
+    };
+
+    const timerId = setTimeout(disableBacklight, BACKLIGHT_TIMEOUT);
+
+    return () => {
+      clearTimeout(timerId);
+      enableBacklight();
     };
   }, []);
 
