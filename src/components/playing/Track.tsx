ce03e90d082ad1bb39575e4a92abe0c75849e2a0
Karl Shea <karl@weilstreet.com>
2023-05-11 00:43:29 -0500

    Transitions

--- src/components/playing/Track.tsx
+++ src/components/playing/Track.tsx
@@ -4,25 +4,36 @@ interface TrackProps {
   track: SpotifyApi.TrackObjectFull;
 }
 
-const Track: React.FC<TrackProps> = ({ track }) => (
-  <div className="currently-playing">
-    <div
-      className="currently-playing--bg"
-      style={{ backgroundImage: `url(${track.album.images[0].url}` }}
-    ></div>
+const Track: React.FC<TrackProps> = ({ track }) => {
+  const image = track.album.images[0];
 
-    <div className="currently-playing--content">
-      <div className="currently-playing--content--inner">
-        <img src={track.album.images[0].url} />
+  return (
+    <div className="currently-playing">
+      <div
+        className="currently-playing--bg"
+        style={{ backgroundImage: `url(${image.url}` }}
+      ></div>
 
-        <h1>{track.name}</h1>
+      <div className="currently-playing--content">
+        <div className="currently-playing--content--inner">
+          <div
+            className="currently-playing--image"
+            style={{
+              backgroundImage: `url(${image.url}`,
+              width: image.width,
+              aspectRatio: `${image.width} / ${image.height}`,
+            }}
+          ></div>
 
-        <h2>{track.artists.map((artist) => artist.name).join(', ')}</h2>
+          <h1>{track.name}</h1>
 
-        <h3>{track.album.name}</h3>
+          <h2>{track.artists.map((artist) => artist.name).join(', ')}</h2>
+
+          <h3>{track.album.name}</h3>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Track;
