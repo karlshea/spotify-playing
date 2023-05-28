297613e4d09307f5d3645a9cf8ab8d76d45b6801
Karl Shea <karl@weilstreet.com>
2023-05-28 15:18:26 -0500

    Move all screen stuff into the hook file

--- src/hooks/useScreenTimeout.ts
+++ src/hooks/useScreenTimeout.ts
@@ -1,8 +1,26 @@
 import { useEffect } from 'react';
-
-import { setScreenEnabled } from '../util/screen.ts';
+import axios from 'axios';
 
 const SCREEN_TIMEOUT = import.meta.env.VITE_SCREEN_TIMEOUT;
+const SCREEN_URL = import.meta.env.VITE_SCREEN_URL;
+const DISABLE_SET_SCREEN = JSON.parse(import.meta.env.VITE_DISABLE_SET_SCREEN);
+
+const setScreenEnabled = (enable: boolean) => {
+  if (DISABLE_SET_SCREEN) {
+    console.info(`${enable ? 'Enabling' : 'Disabling'} screen (suppressed)`);
+    return;
+  }
+
+  console.info(`${enable ? 'Enabling' : 'Disabling'} screen`);
+
+  return axios
+    .get(SCREEN_URL, {
+      params: { enable: enable ? 'on' : 'off' },
+    })
+    .catch((error: unknown) => {
+      console.log(error);
+    });
+};
 
 const useScreenTimeout = (enable: boolean) => {
   // Turn off screen after the timeout, and re-enable it on unmount.
