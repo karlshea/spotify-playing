a4d155a32321873953ab6e58463b716f20875c00
Karl Shea <karl@weilstreet.com>
2023-05-31 01:49:41 -0500

    Fix re-enabled blanking

--- scripts/screen.sh
+++ scripts/screen.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-XAUTHORITY=/home/[username]/.Xauthority /usr/bin/xset -display :0 dpms force ${1}
+if [[ "$1" == "on" ]]
+then
+  XAUTHORITY=/home/[username]/.Xauthority /usr/bin/xset -display :0 dpms force on
+  XAUTHORITY=/home/[username]/.Xauthority /usr/bin/xset -display :0 s noblank
+  XAUTHORITY=/home/[username]/.Xauthority /usr/bin/xset -display :0 s off
+elif [[ "$1" == "off" ]]
+then
+  XAUTHORITY=/home/[username]/.Xauthority /usr/bin/xset -display :0 dpms force off
+fi
