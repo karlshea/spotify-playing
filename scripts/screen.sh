89d4c090bc36adff2e0d11fba582f6b04604684d
Karl Shea <karl@weilstreet.com>
2023-05-31 02:08:21 -0500

    Try again?

--- scripts/screen.sh
+++ scripts/screen.sh
@@ -4,9 +4,9 @@ XAUTHORITY=/home/[username]/.Xauthority
 
 if [[ "$1" == "on" ]]
 then
-  /usr/bin/xset -display :0 dpms force on
   /usr/bin/xset -display :0 s noblank
   /usr/bin/xset -display :0 s off
+  /usr/bin/xset -display :0 -dpms
 elif [[ "$1" == "off" ]]
 then
   /usr/bin/xset -display :0 dpms force off
