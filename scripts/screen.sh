34832a9febe33ded52d5627813feef02d3099660
Karl Shea <karl@weilstreet.com>
2023-06-12 23:24:31 -0500

    Fix screen.sh environment var

--- scripts/screen.sh
+++ scripts/screen.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-XAUTHORITY=/home/[username]/.Xauthority
+XSET="env XAUTHORITY=/home/[username]/.Xauthority /usr/bin/xset -display :0"
 
 if [[ "$1" == "on" ]]
 then
-  /usr/bin/xset -display :0 s noblank
-  /usr/bin/xset -display :0 s off
-  /usr/bin/xset -display :0 -dpms
+  $XSET s noblank
+  $XSET s off
+  $XSET -dpms
 elif [[ "$1" == "off" ]]
 then
-  /usr/bin/xset -display :0 dpms force off
+  $XSET dpms force off
 fi
