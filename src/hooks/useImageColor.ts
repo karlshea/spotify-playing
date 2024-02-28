84109332d874df008214b585bdf642910221c076
Karl Shea <karl@weilstreet.com>
2024-02-27 22:36:42 -0600

    Update to modern-pallete 2

--- src/hooks/useImageColor.ts
+++ src/hooks/useImageColor.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { createPalette } from 'modern-palette';
+import { Palette } from 'modern-palette';
 
 const useImageColor = (canvas: HTMLCanvasElement, loadedUrl: string | null) => {
   const [color, setColor] = useState<string | undefined>();
@@ -10,30 +10,30 @@ const useImageColor = (canvas: HTMLCanvasElement, loadedUrl: string | null) => {
       return;
     }
 
-    const palette = createPalette({
+    const palette = new Palette({
       maxColors: 4,
       samples: [canvas],
     });
 
     // Generate palette colors data
-    palette.generate();
+    palette.generate().then((colors) => {
+      // Find the nearest color on the darker side.
+      const selectedColor = palette.match('#888888')?.color.hex;
 
-    // Find the nearest color on the darker side.
-    const selectedColor = palette.findNearestColor('#888888')?.value;
+      if (import.meta.env.DEV) {
+        console.group('Palette');
+        colors.map((c) => {
+          console.log(
+            `%s%c${c.hex}`,
+            c.hex === selectedColor ? '-> ' : '   ',
+            `background-color: ${c.hex}`
+          );
+        });
+        console.groupEnd();
+      }
 
-    if (import.meta.env.DEV) {
-      console.group('Palette');
-      palette.getColors('hex').map((c) => {
-        console.log(
-          `%s%c${c.value}`,
-          c.value === selectedColor ? '-> ' : '   ',
-          `background-color: ${c.value}`
-        );
-      });
-      console.groupEnd();
-    }
-
-    setColor(selectedColor);
+      setColor(selectedColor);
+    });
   }, [canvas, loadedUrl]);
 
   return color;
